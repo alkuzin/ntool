373a08d0d587cd4535cb0a40b68adcce8d23ea0c
alkuzin <alkuzindev@gmail.com>
2024-09-12 13:09:56 +0300

    feat: added ICMP class

---
+++ include/ntool/icmp.hpp
@@ -0,0 +1,117 @@
+/**
+ * Multifunctional network analyser tool.
+ * Copyright (C) 2024  Alexander (@alkuzin).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file  icmp.hpp
+ * @brief Internet Control Message Protocol wrapper.
+ * 
+ * @author Alexander Kuzin (<a href="https://github.com/alkuzin">alkuzin</a>)
+ * @date   12.09.2024
+ */
+
+#ifndef _NTOOL_ICMP_HPP_
+#define _NTOOL_ICMP_HPP_
+
+#include <netinet/ip_icmp.h>
+#include <cstdint>
+
+namespace ntool {
+
+class ICMP
+{
+    icmphdr m_header; // ICMP header structure
+
+private:
+
+public:
+    /** @brief ICMP object default constructor.*/
+    ICMP(void) = default;
+
+    /**
+     * @brief Construct a new ICMP object.
+     * 
+     * @param [in] header - given ICMP header structure.
+     */
+    ICMP(const icmphdr& header);
+
+    /**
+     * @brief Construct a new ICMP object
+     * 
+     * @param [in] type - given message type.
+     * @param [in] code - given type sub-code.
+     * @param [in] id - given identificator.
+     * @param [in] sequence - given sequence number.
+     */
+    ICMP(std::uint8_t type, std::uint8_t code, std::uint16_t id, std::uint16_t sequence);
+
+    /** @brief ICMP virtual destructor.*/
+    virtual ~ICMP(void) = default;
+
+    /**
+     * @brief Get ICMP header.
+     * 
+     * @return ICMP header. 
+     */
+    icmphdr header(void) const;
+
+    /**
+     * @brief Set the ICMP packet checksum.
+     * 
+     * @return checksum.
+     */
+    std::uint16_t set_checksum(void);
+
+    /**
+     * @brief Get message type.
+     * 
+     * @return ICMP message type.
+     */
+    std::uint8_t type(void) const;
+    
+    /**
+     * @brief Get type sub-code.
+     * 
+     * @return ICMP type sub-code.
+     */
+    std::uint8_t code(void) const;
+
+    /**
+     * @brief Get checksum.
+     * 
+     * @return ICMP checksum.
+     */
+    std::uint16_t checksum(void) const;
+
+    /**
+     * @brief Get identificator.
+     * 
+     * @return ICMP identificator.
+     */
+    std::uint16_t id(void) const;
+
+    /**
+     * @brief Get sequence number.
+     * 
+     * @return ICMP sequence number.
+     */
+    std::uint16_t sequence(void) const;
+};
+
+} // namespace ntool
+
+#endif // _NTOOL_ICMP_HPP_
---
+++ src/icmp.cpp
@@ -0,0 +1,93 @@
+/**
+ * Multifunctional network analyser tool.
+ * Copyright (C) 2024  Alexander (@alkuzin).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <ntool/icmp.hpp>
+#include <cstring>
+
+namespace ntool {
+
+    ICMP::ICMP(const icmphdr& header)
+    {
+        std::memset(&m_header, 0, sizeof(m_header));
+
+        m_header = header;
+        m_header.checksum = set_checksum();
+    }
+
+    ICMP::ICMP(std::uint8_t type, std::uint8_t code, std::uint16_t id, std::uint16_t sequence)
+    {
+        std::memset(&m_header, 0, sizeof(m_header));
+        
+        m_header.type             = type;
+        m_header.code             = code;
+        m_header.un.echo.id       = id;
+        m_header.un.echo.sequence = sequence;
+        m_header.checksum         = set_checksum();
+    }
+    
+    std::uint16_t ICMP::set_checksum(void)
+    {
+        std::uint16_t *buf   = reinterpret_cast<std::uint16_t *>(&m_header);
+        std::size_t   len    = sizeof(m_header);
+        std::uint32_t sum    = 0;
+        std::uint16_t result = 0;
+
+        for (sum = 0; len > 1; len -= 2)
+            sum += *buf++;
+
+        if (len == 1)
+            sum += *reinterpret_cast<std::uint8_t *>(buf);
+        
+        sum     = (sum >> 0x10) + (sum & 0xFFFF);
+        sum    += (sum >> 0x10);
+        result  = ~sum;
+
+        return result;
+    }
+
+    icmphdr ICMP::header(void) const
+    {
+        return m_header;
+    }
+
+    std::uint8_t ICMP::type(void) const
+    {
+        return m_header.type;
+    }
+    
+    std::uint8_t ICMP::code(void) const
+    {
+        return m_header.code;
+    }
+
+    std::uint16_t ICMP::checksum(void) const
+    {
+        return m_header.checksum;
+    }
+
+    std::uint16_t ICMP::id(void) const
+    {
+        return m_header.un.echo.id;
+    }
+
+    std::uint16_t ICMP::sequence(void) const
+    {
+        return m_header.un.echo.sequence;
+    }
+
+} // namespace ntool
