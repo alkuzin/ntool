5f7293ef60fd0ec973e8834c033b559aa4b3f2f9
alkuzin <alkuzindev@gmail.com>
2025-08-28 16:16:29 +0300

    [feat]: added ntool-ping crate

---
+++ ntool-ping/src/lib.rs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0.
+// Copyright (C) 2025-present ntool project and contributors.
+
+//! Network diagnostic tool used to test the reachability
+//! of a host on an Internet Protocol (IP) network.
+
+/// Ping specific target.
+///
+/// # Parameters
+/// - `target` - given target IP address/hostname to ping.
+/// - `count` - given number of pings.
+pub fn ping(_target: &str, _count: u8) {
+    unimplemented!()
+}
