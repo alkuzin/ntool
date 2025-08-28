b1270925e48ac5268e9238e5ed432e862be0ed8d
alkuzin <alkuzindev@gmail.com>
2025-08-28 13:24:32 +0300

    [feat]: added project CLI parser module

---
+++ ntool/src/cli.rs
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0.
+// Copyright (C) 2025-present ntool project and contributors.
+
+//! CLI (Command-Line Interface) commands related declarations.
+
+use crate::config::{DESCRIPTION, NAME, VERSION};
+use chrono::Datelike;
+use clap::Parser;
+
+/// Command-line arguments struct.
+#[derive(Parser)]
+pub struct Cli {
+    /// Version flag.
+    #[clap(short, long, help = "Display project version")]
+    pub version: bool,
+}
+
+/// Parse the command-line arguments.
+///
+/// # Return
+/// - Parsed command-line arguments.
+pub fn parse() -> Cli {
+    let args = Cli::parse();
+    args
+}
+
+/// Display project version.
+pub fn version() {
+    let year = chrono::Utc::now().year();
+    let copyright =
+        format!("Copyright (C) {year} ntool project and contributors.");
+
+    println!("{} {} - {}.\n{}", NAME, VERSION, DESCRIPTION, copyright);
+}
